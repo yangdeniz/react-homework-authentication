ffca74eab39d3e3ad64e4ea939dc394b46ab1d54
Marina Sunko <32299890+yangdeniz@users.noreply.github.com>
2022-09-12 22:45:14 +0200

    Delete frontend directory

--- frontend/src/components/header/logo.js
+++
@@ -1,5 +0,0 @@
-function Logo() {
-  return <div className='logo'>Neto Social</div>
-}
-
-export default Logo;
--- frontend/src/shared/constants/endpoints.js
+++
@@ -1,7 +0,0 @@
-const ENDPOINTS = {
-  AUTH: 'auth',
-  PROFILE: 'private/me',
-  NEWS: 'private/news'
-}
-
-export default ENDPOINTS;
